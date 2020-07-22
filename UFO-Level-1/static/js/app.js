254b33793a54bcc76b8af4332c558f00618ccb07
Meera <meera@Meeras-MacBook-Air-2.local>
2020-07-22 14:43:15 -0500

    final commit

--- UFO-Level-1/static/js/app.js
+++ UFO-Level-1/static/js/app.js
@@ -1,19 +1,49 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-console.log(data);
-function createTable(data) {
-    tbody.html("")
-}
+// Get a reference to the table body
+var tbody = d3.select("tbody");
 
-createTable(data)
+// Console.log the UFO sightings data from data.js
+console.log(tableData);
 
-function handleClick() {
-    var date = d3.select("#datetime").property("value");
-    var filterData = tableData;
-    filterData = filterData.filter(row=>row.datetime===date);
-    createTable(filterData)
-}
+// Append a table to web page that adds new row per sighting
+tableData.forEach((data) => {
+    var row = tbody.append("tr");
+    Object.entries(data).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
 
-d3.selectAll("#filter-btn").on("click", handleClick);
+// Add event listener and search through date/time column to find rows that match user input
+// Select button
+var button = d3.select("#filter-btn");
+
+button.on("click", function() {
+    // Select the input element and get the raw HTML code
+    var inputElement = d3.select("#datetime");
+
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+
+    var filteredData = tableData.filter(date => date.datetime === inputValue);
+
+    console.log(filteredData);
+    console.log(tableData);
+
+    // Select unordered list element by class name
+    var list = d3.select("tbody");
+
+    list.html("");
+
+    filteredData.forEach((data) => {
+        var row = tbody.append("tr");
+        Object.entries(data).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
